d0715eaed1b3e88fa17c9ca0026b8684515952e9
mekadalibrahem <mekadalibrahem123@gmail.com>
2025-12-10 20:00:14 +0300

    fic typo

--- app/Modules/Works/Policies/WorkRequirmentPolicy.php
+++ app/Modules/Works/Policies/WorkRequirmentPolicy.php
@@ -5,7 +5,7 @@ namespace App\Modules\Works\Policies;
 
 use App\Models\User;
 use App\Modules\Works\Entities\Models\Work;
-use App\Modules\Works\Entities\Models\WorkRequirment;
+use App\Modules\Works\Entities\Models\WorkRequirement;
 use Illuminate\Auth\Access\Response;
 
 class WorkRequirmentPolicy
@@ -21,7 +21,7 @@ class WorkRequirmentPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, WorkRequirment $workRequirment): bool
+    public function view(User $user, WorkRequirement $workRequirment): bool
     {
         return true;
     }
@@ -38,7 +38,7 @@ class WorkRequirmentPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Work $work, WorkRequirment $workRequirment): bool
+    public function update(User $user, Work $work, WorkRequirement $workRequirment): bool
     {
         if ($work->isOwnedBy($user) && $work->id = $workRequirment->work_id) {
             return true;
@@ -49,7 +49,7 @@ class WorkRequirmentPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Work $work, WorkRequirment $workRequirment): bool
+    public function delete(User $user, Work $work, WorkRequirement $workRequirment): bool
     {
         if ($work->isOwnedBy($user) && $work->id = $workRequirment->work_id) {
             return true;
@@ -60,7 +60,7 @@ class WorkRequirmentPolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Work $work, WorkRequirment $workRequirment): bool
+    public function restore(User $user, Work $work, WorkRequirement $workRequirment): bool
     {
         if ($work->isOwnedBy($user) && $work->id = $workRequirment->work_id) {
             return true;
@@ -71,7 +71,7 @@ class WorkRequirmentPolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Work $work, WorkRequirment $workRequirment): bool
+    public function forceDelete(User $user, Work $work, WorkRequirement $workRequirment): bool
     {
         if ($work->isOwnedBy($user) && $work->id = $workRequirment->work_id) {
             return true;
